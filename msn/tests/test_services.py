3bad0accdcc82a993418862e5a12b8987c782d4b
McArthur Leger <mcarthur.leger@customeraffairs.com>
2017-08-07 00:00:39 -0300

    adding tests to our project

--- msn/tests/test_services.py
+++ msn/tests/test_services.py
@@ -13,9 +13,18 @@ class ServiceTest(TestCase):
         extract.return_value = {}
         result = self.services.get_weather()
         self.assertEquals(result, {})
-    
+
     def test_extract_info(self):
-        pass
+        data = {
+            'url': 'http//test.com',
+            'weather': [
+                {'icon': 'icon'}],
+            'main': {'temp': 0, 'humidity': 0},
+            'temperature': 0
+        }
+        weather = self.services.extract_info(data)
+        self.assertEquals(weather['humidity'], 0)
 
     def test_get_forms(self):
-        pass
+        result = self.services.get_forms()
+        self.assertEquals(result.count(), 0)
